31a27d699d2ce39285decb36d0a6284b239aad3c
adelElsawaf <adelsadeq24@gmail.com>
2024-11-29 14:52:43 +0200

    chore(project) : intialize project

---
+++ src/main/java/com/flashcard/service/FlashCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flashcard.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashcard/service/FlashCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcard.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
